c7eca23a54074244ea90e509a778500c823d65b3
Zawaw1 <158816467+Zawaw1@users.noreply.github.com>
2024-02-04 19:16:12 +0800

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate',methods=['POST'])
+
+def translate():
+    try:
+        subprocess.run(['python','C:\\Users\\USER\\Desktop\\VM Evaluate\\src\\Test_mouse.py'])
+        return render_template('index.html')
+
+    except Exception as e:
+        return render_template('404.html', error_message=str(e))
+    
+if __name__ == '__main__':
+    app.run(debug=True)
